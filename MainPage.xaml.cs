0f2a53eba100cb281d64dfb8b4aa15d9a200afb2
jiaorem <yabut.micohjomarie@auf.edu.ph>
2024-08-21 14:04:38 +0800

    Add project files.

---
+++ MainPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿namespace Module01Exercise01
+{
+    public partial class MainPage : ContentPage
+    {
+        public string Firstname { get; set; }
+        public Color ColorIndigo { get; set; }
+        public MainPage()
+        {
+            InitializeComponent();
+
+            Firstname = "Hello There!";
+            ColorIndigo = Colors.Indigo;
+
+            this.BindingContext = this;
+        }
+
+        private void SaveButton_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void OnChangeBackgroundColorL(object sender, EventArgs e)
+        {
+            this.Resources["LocalBackgroundColor"] = Colors.GreenYellow;
+        }
+
+        private void OnChangeBackgroundColorG(object sender, EventArgs e)
+        {
+            this.Resources["LocalBackgroundColor"] = Colors.Gold;
+        }
+
+        private void OnChangeBackgroundColorR(object sender, EventArgs e)
+        {
+            this.Resources["LocalBackgroundColor"] = Colors.Red;
+        }
+
+
+    }
+
+}
